19d08327bf78b1c03d8f4950f42193427f02ee72
adrianometalslug <sdcomputadores@gmail.com>
2018-03-29 09:26:22 -0300

    add array.of

---
+++ Array.of.js
@@ -0,0 +1,12 @@
+/*
+ * Cria array com os elementos passados
+ */
+
+console.log(Array.of(1));
+//[1]
+
+console.log(Array.of(1, 2, 3));
+//[1, 2, 3]
+
+console.log(Array.of(undefined));
+//[undefined]
